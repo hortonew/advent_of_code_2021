4922eb98d58d65984c136ce8ea7dc2a9b6809646
Erik Horton <hortonew@gmail.com>
2021-12-05 15:09:15 -0700

    Day 5 Exercise 1 complete

---
+++ day_5/main_5_1.py
@@ -0,0 +1,122 @@
+"""Hydrothermal Venture."""
+from dataclasses import dataclass
+from typing import Dict, List, Tuple
+
+
+@dataclass
+class Coordinate:
+    x: int
+    y: int
+
+
+def is_line(coordinate_1: Coordinate, coordinate_2: Coordinate):
+    """Return true if x1==x2, or y1==y2 match."""
+    return coordinate_1.x == coordinate_2.x or coordinate_1.y == coordinate_2.y
+
+
+def get_coordinates_from_range(
+    coordinate_1: Coordinate, coordinate_2: Coordinate
+) -> List[Coordinate]:
+    """Return list of coordinates that fall in range."""
+    '''
+    Return list of coordinates that fall in range.
+
+    Example:
+        1,1 -> 1,3: Returns [(1, 1), (1, 2), (1, 3)]
+        9,7 -> 7,7: Returns [(9, 7), (8, 7), (7, 7)]
+    '''
+    start_x = coordinate_1.x
+    start_y = coordinate_1.y
+    end_x = coordinate_2.x
+    end_y = coordinate_2.y
+    points: List[Coordinate] = []
+
+    if start_x == end_x:
+        # do y line
+        begin = min(start_y, end_y)
+        end = max(start_y, end_y)
+        point_range = list(range(begin, end)) + [end]
+
+        if begin == end_y:
+            point_range.reverse()
+        for point in point_range:
+            points.append(Coordinate(start_x, point))
+    elif start_y == end_y:
+        # do x line
+        begin = min(start_x, end_x)
+        end = max(start_x, end_x)
+        point_range = list(range(begin, end)) + [end]
+
+        if begin == end_x:
+            point_range.reverse()
+        for point in point_range:
+            points.append(Coordinate(point, start_y))
+    else:
+        # return single point as they're equal
+        return [Coordinate(start_x, end_y)]
+
+    return points
+
+
+def convert_file_to_data(file_name: str):
+    """Loads raw input into list."""
+    with open(file_name, 'r') as f:
+        items = f.read().splitlines()
+
+    return items
+
+
+def create_coordinate_tuple_from_raw(
+    input: str,
+) -> Tuple[Coordinate, Coordinate]:
+    """Take raw input and convert into two coordinates."""
+    parse = input.split(' -> ')
+    c1 = parse[0].split(',')
+    c2 = parse[1].split(',')
+    return Coordinate(int(c1[0]), int(c1[1])), Coordinate(
+        int(c2[0]), int(c2[1])
+    )
+
+
+def convert_raw_input_to_acceptable_points(
+    raw_ranges: List[str],
+) -> List[Coordinate]:
+    """Builds list of acceptable data points that meet criteria."""
+    acceptable_points: List[Coordinate] = []
+    for range in raw_ranges:
+        c1, c2 = create_coordinate_tuple_from_raw(range)
+        if is_line(c1, c2):
+            points = get_coordinates_from_range(c1, c2)
+            for point in points:
+                acceptable_points.append(point)
+
+    return acceptable_points
+
+
+def get_count_of_overlapping_points(coordinates: List[Coordinate]) -> int:
+    """Measures how many points overlap more than once."""
+    counts: Dict[str, int] = {}
+    for coordinate in coordinates:
+        try:
+            counts[f'{coordinate.x},{coordinate.y}'] += 1
+        except KeyError:
+            counts[f'{coordinate.x},{coordinate.y}'] = 1
+
+    return sum(value >= 2 for point, value in counts.items())
+
+
+def main():
+    """Return count of locations that have at least two numbers overlapping."""
+    # raw input to list of items
+    raw_input = convert_file_to_data('5.txt')
+
+    # list of items to range of coordinates
+    acceptable_points = convert_raw_input_to_acceptable_points(raw_input)
+    count_of_overlapping_points = get_count_of_overlapping_points(
+        acceptable_points
+    )
+    print(count_of_overlapping_points)
+
+
+if __name__ == '__main__':
+    main()
---
+++ day_5/tests/test_5.py
@@ -0,0 +1,128 @@
+import pytest
+from day_5.main_5_1 import (
+    Coordinate,
+    convert_file_to_data,
+    convert_raw_input_to_acceptable_points,
+    create_coordinate_tuple_from_raw,
+    get_coordinates_from_range,
+    get_count_of_overlapping_points,
+)
+
+
+@pytest.fixture
+def sample():
+    return [
+        '0,9 -> 5,9',
+        '8,0 -> 0,8',
+        '9,4 -> 3,4',
+        '2,2 -> 2,1',
+        '7,0 -> 7,4',
+        '6,4 -> 2,0',
+        '0,9 -> 2,9',
+        '3,4 -> 1,4',
+        '0,0 -> 8,8',
+        '5,5 -> 8,2',
+    ]
+
+
+@pytest.fixture
+def acceptable_points():
+    return [
+        # 0, 9 -> 5, 9
+        Coordinate(0, 9),
+        Coordinate(1, 9),
+        Coordinate(2, 9),
+        Coordinate(3, 9),
+        Coordinate(4, 9),
+        Coordinate(5, 9),
+        # skip
+        # 9, 4 -> 3, 4
+        Coordinate(9, 4),
+        Coordinate(8, 4),
+        Coordinate(7, 4),
+        Coordinate(6, 4),
+        Coordinate(5, 4),
+        Coordinate(4, 4),
+        Coordinate(3, 4),
+        # 2, 2 -> 2, 1
+        Coordinate(2, 2),
+        Coordinate(2, 1),
+        # 7, 0 -> 7, 4
+        Coordinate(7, 0),
+        Coordinate(7, 1),
+        Coordinate(7, 2),
+        Coordinate(7, 3),
+        Coordinate(7, 4),
+        # skip
+        # 0, 9 -> 2, 9
+        Coordinate(0, 9),
+        Coordinate(1, 9),
+        Coordinate(2, 9),
+        # 3, 4 -> 1, 4
+        Coordinate(3, 4),
+        Coordinate(2, 4),
+        Coordinate(1, 4),
+        # skip
+        # skip
+    ]
+
+
+def test_5_convert_file_to_data(sample):
+    raw_input = convert_file_to_data('day_5/tests/sample.txt')
+    assert raw_input == sample
+
+
+def test_5_create_coordinate_tuple_from_raw():
+    raw_input = '0,9 -> 5,9'
+    c1, c2 = create_coordinate_tuple_from_raw(raw_input)
+    assert c1 == Coordinate(0, 9)
+    assert c2 == Coordinate(5, 9)
+
+
+def test_5_get_coordinates_from_range():
+    c1 = Coordinate(1, 1)
+    c2 = Coordinate(1, 3)
+    points = get_coordinates_from_range(c1, c2)
+    assert points == [
+        Coordinate(1, 1),
+        Coordinate(1, 2),
+        Coordinate(1, 3),
+    ]
+
+
+def test_5_get_coordinates_from_range_2():
+    c1 = Coordinate(9, 7)
+    c2 = Coordinate(7, 7)
+    points = get_coordinates_from_range(c1, c2)
+    assert points == [
+        Coordinate(9, 7),
+        Coordinate(8, 7),
+        Coordinate(7, 7),
+    ]
+
+
+def test_5_get_coordinates_from_range_3():
+    c1 = Coordinate(0, 4)
+    c2 = Coordinate(0, 1)
+    points = get_coordinates_from_range(c1, c2)
+    assert points == [
+        Coordinate(0, 4),
+        Coordinate(0, 3),
+        Coordinate(0, 2),
+        Coordinate(0, 1),
+    ]
+
+
+def test_5_convert_raw_input_to_acceptable_points(sample, acceptable_points):
+    assert convert_raw_input_to_acceptable_points(sample) == acceptable_points
+
+
+def test_5_get_count_of_overlapping_points():
+    assert (
+        get_count_of_overlapping_points([Coordinate(1, 1), Coordinate(1, 1)])
+        == 1
+    )
+
+
+def test_5_get_count_of_overlapping_points_sample(acceptable_points):
+    assert get_count_of_overlapping_points(acceptable_points) == 5
